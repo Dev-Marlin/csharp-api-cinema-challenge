fb061b99c02cfb7450cbb22199f75402221bb905
Martin Lind <marlin00.Work@gmail.com>
2025-01-28 16:21:18 +0100

    First push, created ER and made the models

---
+++ api-cinema-challenge/api-cinema-challenge/Models/Ticket.cs
@@ -0,0 +1,11 @@
+﻿namespace api_cinema_challenge.Models
+{
+    public class Ticket
+    {
+        public int ScreeningId { get; set; }
+        public int CustomerId { get; set; }
+        public float Price { get; set; }
+        public DateTime BookingDate { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
