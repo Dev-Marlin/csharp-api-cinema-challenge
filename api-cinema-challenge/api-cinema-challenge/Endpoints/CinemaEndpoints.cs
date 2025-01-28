723d33f5598eb9204f8caeb85196a460dc883489
Martin Lind <marlin00.Work@gmail.com>
2025-01-28 16:41:47 +0100

    Started on the endpoints

---
+++ api-cinema-challenge/api-cinema-challenge/Endpoints/CinemaEndpoints.cs
@@ -0,0 +1,23 @@
+﻿namespace api_cinema_challenge.Endpoints
+{
+    public static class CinemaEndpoints
+    {
+        public static void ConfigureEndpoints(this WebApplication app)
+        {
+            var cinema = app.MapGroup("/cinema");
+
+            cinema.MapPost("/customer/create", CreateCustomer);
+            cinema.MapGet("/customer/all", GetCustomers);
+            cinema.MapPut("/customer/update/{id}", UpdateCustomer);
+            cinema.MapDelete("/customer/delete/{id}", DeleteCustomer);
+
+            cinema.MapPost("/movie/create", CreateMovie);
+            cinema.MapGet("/movie/all", GetMovies);
+            cinema.MapPut("/movie/update/{id}", UpdateMovie);
+            cinema.MapDelete("/movie/delete/{id}", DeleteMovie);
+
+            cinema.MapPost("/screening/create", CreateScreening);
+            cinema.MapGet("/screening/all", GetScreenings);
+        }
+    }
+}
